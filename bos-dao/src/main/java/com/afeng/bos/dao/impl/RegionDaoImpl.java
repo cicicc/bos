a5956c37109a20e10fbb33f5dbdc0b0139898e75
Chen Feng <afeng3990@163.com>
2018-05-21 23:15:28 +0800

    #一键上传代码的测试

--- bos-dao/src/main/java/com/afeng/bos/dao/impl/RegionDaoImpl.java
+++ bos-dao/src/main/java/com/afeng/bos/dao/impl/RegionDaoImpl.java
@@ -2,6 +2,8 @@ package com.afeng.bos.dao.impl;
 
 import com.afeng.bos.dao.IRegionDao;
 import com.afeng.bos.domain.Region;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public class RegionDaoImpl extends BaseDaoImpl<Region> implements IRegionDao  {
 }
